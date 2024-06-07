9c3a1cf51bf4e02c7975384ba4ccc61c63daff4a
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-07 11:27:55 -0300

    feat: adiciona vários cadastros

--- js/scripts.js
+++ js/scripts.js
@@ -30,18 +30,6 @@ function cadastrar() {
     alert('Contato salvo com sucesso!');
 }
 
-// Teste: adaptar a exibição em formulário html
-/**
-function exibir() {
-    console.log("Meu cadastro é: ");
-    for (let i = 0; i < cadastros.length; i++) {
-        console.log(cadastros[i].nome);
-        console.log(cadastros[i].cpf);
-        console.log(cadastros[i].dataNascimento);
-        console.log(cadastros[i].endereco);
-    }
-}
- */
 
 /**
  * Função exibir contatos
@@ -55,24 +43,28 @@ function exibir() {
     var divCadastro = document.createElement('div');
     
     // Cria div's filhas
-    var divNome = document.createElement('div');
-    var divCpf = document.createElement('div');
-    var divData = document.createElement('div');
-    var divEndereco = document.createElement('div');
+    for (let i = 0; i < cadastros.length; i++) {
+        var divNome = document.createElement('div');
+        var divCpf = document.createElement('div');
+        var divData = document.createElement('div');
+        var divEndereco = document.createElement('div');
+    }
 
     // Seta dados nas div's filhas
-    divNome.textContent = cadastros[0].nome;
-    divCpf.textContent = cadastros[0].cpf;
-    divData.textContent = cadastros[0].dataNascimento;
-    divEndereco.textContent = cadastros[0].endereco;
+    for (let i = 0; i < cadastros.length; i++) {
+        divNome.textContent = cadastros[i].nome;
+        divCpf.textContent = cadastros[i].cpf;
+        divData.textContent = cadastros[i].dataNascimento;
+        divEndereco.textContent = cadastros[i].endereco;
+    }
 
-    // Teste: Adiciona dados na div 'divCadastro'
-    // Obs.: exibe apenas dados de uma pessoa.
-    // Implementar loop para exibir todos
-    divCadastro.appendChild(divNome);
-    divCadastro.appendChild(divCpf);
-    divCadastro.appendChild(divData);
-    divCadastro.appendChild(divEndereco);
+    // Adiciona dados na div 'divCadastro'
+    for (let i = 0; i < cadastros.length; i++) {
+        divCadastro.appendChild(divNome);
+        divCadastro.appendChild(divCpf);
+        divCadastro.appendChild(divData);
+        divCadastro.appendChild(divEndereco);
+    }
 
     // Adiciona id e altera/adiciona CSS na div 'divCadastro':
     divCadastro.setAttribute('id', 'divCad');
@@ -81,26 +73,3 @@ function exibir() {
     article.appendChild(divCadastro);
 }
 
-/** Modelo: adicionar CSS a função exibir:
-<div id="cadastro">
-    <div id="">João Paulo da Silva</div>
-    <div>CPF: 999.999.999-99</div>
-    <div>Data Nascimento: 11/11/2010</div>
-    <div>Endereço: Rua B, Bela Vista, Natal/RN</div>
-</div>
- */
-
-// testes:
-/**
-function exibir() {
-    console.log("Meu cadastro é: ");
-    for (const key in cadastros) {
-        for (const indice in lista) {
-            if (Object.hasOwnProperty.call(lista, indice)) {
-                const element = lista[indice];
-                console.log(element);
-            }       
-        }
-    }
-}
-*/
